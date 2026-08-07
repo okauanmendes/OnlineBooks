33fd94c68a7bf2727fe0c51bc8da61e2e891240f
Kauan Mendes <kauanmendesivo180@gmail.com>
2026-08-07 20:33:41 -0300

    feat: adiciona classe principal da aplicação

---
+++ src/main/java/com/audiobook/app/AudiobookAppApplication.java
@@ -0,0 +1,12 @@
+﻿package com.audiobook.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiobookAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudiobookAppApplication.class, args);
+    }
+}
