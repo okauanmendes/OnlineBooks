7df704dd4f99a295031e11e85642f09017d8cfab
Kauan Mendes <kauanmendesivo180@gmail.com>
2026-08-07 20:26:38 -0300

    feat: implementa autenticação e JWT

---
+++ src/main/java/com/audiobook/app/security/JwtService.java
@@ -0,0 +1,15 @@
+﻿package com.audiobook.app.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String createToken(String username) {
+        return "token";
+    }
+
+    public boolean validateToken(String token) {
+        return true;
+    }
+}
